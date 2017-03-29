5a708b76be8b91da994cb05544139e758c122390
Zeko369 <zekan.fran369@gmail.com>
2017-03-29 15:20:57 +0200

    Added c++ to for eval

--- test_for_2/eval_for.sh
+++ test_for_2/eval_for.sh
@@ -5,16 +5,26 @@ echo "Name of the file"
 read name
 max=5
 
+if [ "${name: -1}" = "p" ]; then
+	pero=${name:0:(-4)}
+	g++ -DEVAL -O2 -o $pero $pero.cpp
+fi
+
 for i in `seq 1 $max`;
 do
+	if [ "${name: -1}" = "y" ]; then
+		pero=${name:0:(-3)}
+		python $pero.py < $pero.in.$i > $pero.out.$i
+		diff $pero.out.$i $pero.sup.$i> resault.txt
+	else
+		./$pero < $pero.in.$i > $pero.out.$i
+		diff $pero.out.$i $pero.sup.$i > resault.txt
+	fi
 
-python $name.py < $name.in.$i > $name.out.$i 
-diff $name.out.$i $name.sup.$i > resault.txt
-
-var=$(cat resault.txt)
-if [ "$var" = "" ]; then
-	echo "Nice, perfect"
-else
-	echo $var
-fi
+	var=$(cat resault.txt)
+	if [ "$var" = "" ]; then
+		echo "Nice, perfect"
+	else
+		echo $var
+	fi
 done
